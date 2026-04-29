d8af20c92c9d75c96039561eee803fba77723f6e
Em <158059167+Laglue-em@users.noreply.github.com>
2026-04-29 11:47:59 +0200

    Initialize main.lua for Pong game

---
+++ main.lua
@@ -0,0 +1,36 @@
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+push = require 'push'
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest','nearest')
+    largeFont = love.graphics.newFont('font.ttf', 32)
+    smallFont = love.graphics.newFont('font.ttf', 8)
+    
+    
+    love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
+        resizable = false,
+        vsync = true,
+        fullscreen = false,
+    })
+    push.setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, {upscale ="normal"})
+end
+
+function love.keypressed(key)
+    if key == 'escape' then 
+        love.event.quit()
+    end
+end
+    
+
+function love.draw()
+    push.start()
+    love.graphics.clear(110/255, 35/255, 51/255, 1)
+    love.graphics.setFont(largeFont)
+    love.graphics.printf("Hello, Pong!", 0, VIRTUAL_HEIGHT/2-6, VIRTUAL_WIDTH, 'center')
+    push.finish()
+end
